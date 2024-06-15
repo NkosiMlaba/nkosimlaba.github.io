8ab44dec8511c2271d1dc6449901486e180bdd76
Nkosikhona Mlaba <nkosimlaba397@gmail.com>
2024-06-15 18:49:06 +0200

    added better support for mobile display picture viewing

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
@@ -12,3 +13,31 @@ function expand(previous_section, current_section){
 
   current_section.style.marginTop = section1Height + "px";
 }
+
+// Get all the image elements
+const images = document.querySelectorAll('img');
+const fullscreen = document.getElementById('fullscreen');
+const fullscreenImage = document.getElementById('fullscreen-image');
+
+// Attach click event listener to each image
+images.forEach(function(image) {
+    image.addEventListener('click', function() {
+        openFullscreen(image.src);
+    });
+});
+
+// Function to open the image in fullscreen
+function openFullscreen(src) {
+    // Set the src of the fullscreen image to the clicked image
+    fullscreenImage.src = src;
+    
+    // Show the fullscreen element
+    fullscreen.style.display = 'flex';
+}
+
+// Close the fullscreen when clicking outside the image
+fullscreen.addEventListener('click', function(event) {
+    if (event.target === fullscreen) {
+        fullscreen.style.display = 'none';
+    }
+});
