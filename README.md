923b579faf018ef755b89bd2fd2d4c83d9aa2124
Nkosikhona Mlaba <nkosimlaba397@gmail.com>
2024-07-13 20:18:38 +0200

    removing white spaces, formatting another way

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Minimal-Website
-
+An implementation of a website that contains information about me and projects I have worked on 
 
 
--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
 }
 
-function expand(previous_section, current_section){
-  var section1 = document.getElementById(previous_section)
-  
-  var section1Height = section1.offsetHeight;
+function expand(previous_section, current_section) {
+    var section1 = document.getElementById(previous_section)
 
-  current_section.style.marginTop = section1Height + "px";
+    var section1Height = section1.offsetHeight;
+
+    current_section.style.marginTop = section1Height + "px";
 }
 
 // Get all the image elements
-const images = document.querySelectorAll('img');
+const images = document.querySelectorAll('img:not(.icon)');
 const fullscreen = document.getElementById('fullscreen');
 const fullscreenImage = document.getElementById('fullscreen-image');
 
 // Attach click event listener to each image
-images.forEach(function(image) {
-    image.addEventListener('click', function() {
+images.forEach(function (image) {
+    image.addEventListener('click', function () {
         openFullscreen(image.src);
     });
 });
@@ -30,13 +30,13 @@ images.forEach(function(image) {
 function openFullscreen(src) {
     // Set the src of the fullscreen image to the clicked image
     fullscreenImage.src = src;
-    
+
     // Show the fullscreen element
     fullscreen.style.display = 'flex';
 }
 
 // Close the fullscreen when clicking outside the image
-fullscreen.addEventListener('click', function(event) {
+fullscreen.addEventListener('click', function (event) {
     if (event.target === fullscreen) {
         fullscreen.style.display = 'none';
     }
